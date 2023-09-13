d5e23344cdcf0f1e9fe9df811094cc84c4a2c36a
Timii <106264110+iitimii@users.noreply.github.com>
2023-09-13 23:58:35 +0100

    Update file_api.py

--- file_api.py
+++ file_api.py
@@ -55,4 +55,4 @@ async def upload_files(flair: UploadFile, t1ce: UploadFile):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
